a7b8abb75be647d3ab7dc8589f1bbd81d9107da7
Lakshan <lcjayaweera@gmail.com>
2023-07-17 20:11:42 +0900

    getElementByID

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
-INSERT INTO alien  VALUES (101,'Lakshan');
+INSERT INTO alien  VALUES (101,'Lakshan','Java');
+INSERT INTO alien  VALUES (102,'Chathura','C');
+INSERT INTO alien  VALUES (103,'Jayaweera','Python');
+INSERT INTO alien  VALUES (104,'Supun','C++');
+INSERT INTO alien  VALUES (105,'Saman','Android');
+
